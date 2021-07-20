320dde965ee2a0c0eeed5767b9560abcb24c1b54
luizarilo <luiz.arilo@gmail.com>
2021-07-20 09:54:16 -0300

    Inserção do texto nas fotos do modal e modificação do favicon

--- js/scripts.js
+++ js/scripts.js
@@ -43,12 +43,12 @@ window.addEventListener('DOMContentLoaded', event => {
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
-    /* responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
             }
         });
-    }); */
+    });
 
 });
